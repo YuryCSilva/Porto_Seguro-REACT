3265720f6a3429063444c0e9e2e1f600da2bb73e
YuryCSILVA <yury.csilvaa@gmail.com>
2022-03-14 21:39:37 -0300

    revert: re-edit Index Styled Component

--- src/components/UI/Menu/index.tsx
+++ src/components/UI/Menu/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container } from "./styles";
 
-const Menu: React.FC = (children) => <Container>{children}</Container>
+const Menu: React.FC = ({children}) => <Container>{children}</Container>
 
 export default Menu;
