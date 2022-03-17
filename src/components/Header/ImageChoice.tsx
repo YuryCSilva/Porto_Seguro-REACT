11fa7264a4c49ba2f117e6965cb49985f093a8d3
YuryCSILVA <yury.csilvaa@gmail.com>
2022-03-16 22:04:22 -0300

    feat: add New Components
    [

---
+++ src/components/Header/ImageChoice.tsx
@@ -0,0 +1,15 @@
+interface ImageChoiceProps {
+  srcSetMobile: string;
+  srcSetDesktop: string;
+  src: string;
+}
+
+export function ImageChoice(props: ImageChoiceProps) {
+  return (
+    <picture>
+      <source media="(max-width: 1023px)" srcSet={props.srcSetMobile} />
+      <source media="(min-width: 1024px)" srcSet={props.srcSetDesktop} />
+      <img src={props.src} alt="Logo Norte a Sul" />
+    </picture>
+  );
+}
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,188 @@
+import styled from "styled-components";
+
+export const Menu = styled.div`
+  display: flex;
+  padding:  14px 21px 16px 21px;
+  max-height: 60px;
+  box-sizing: border-box;
+  align-items: center;
+  justify-content: space-between;
+  box-shadow: 0px 5px 6px #00000024;
+  background: #FEFEFE 0% 0% no-repeat padding-box;
+
+  text-transform: uppercase;
+
+  div.imageContainer picture:first-child {
+    padding-right: 5.33vw;
+  }
+
+  div.imageContainer picture:nth-child(2) {
+    padding-left: 5.33vw;
+  }
+
+  @media (min-width: 1024px) {
+    max-height: 92px;
+    justify-content: space-between;
+    padding: 24px 50px 25px 50px;
+    div.imageContainer {
+      display: flex;
+      align-items: center;
+    }
+
+    div.imageContainer picture:first-child {
+      padding-right: 3.84vw;
+    }
+
+    div.imageContainer picture:nth-child(2) {
+      padding-left: 3.84vw;
+    }
+  }
+`;
+
+interface ToggleProps {
+  active: boolean;
+}
+
+export const Toggle = styled.a<ToggleProps>`
+  display: flex;
+  flex-direction: column;
+  justify-content: ${(props) => (props.active ? "center" : "space-between")};
+  width: 35px;
+  height: 22px;
+  cursor: pointer;
+  position: relative;
+  span {
+    content: "";
+    display: block;
+    height: 2px;
+    width: 100%;
+    background-color: #000;
+    transition: 0.3s;
+  }
+
+  span:first-child {
+    transform: rotate(${(props) => (props.active ? "135deg" : "0")});
+    position: ${(props) => (props.active ? "absolute" : "static")};
+    left: -0.5px;
+  }
+  span:nth-child(2) {
+    display: ${(props) => (props.active ? "none" : "block")};
+  }
+  span:nth-child(3) {
+    transform: rotate(${(props) => (props.active ? "-135deg" : "0")});
+    position: ${(props) => (props.active ? "absolute" : "static")};
+    left: 0.5px;
+  }
+  @media (min-width: 1024px) {
+    display: none;
+  }
+`;
+
+export const MenuNav = styled.nav<ToggleProps>`
+  position: fixed;
+  width: 100vw;
+  height: calc(100% - 60px);
+  background-color: #9a6d0c;
+  right: ${(props) => (props.active ? "0" : "-100%")};
+  top: 60px;
+  color: #fff;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  transition: 0.6s;
+
+  ul {
+    li {
+      font-size: 21px;
+      letter-spacing: 4.2px;
+      border-bottom: 1px solid #efc566;
+
+      a {
+        width: 100%;
+        padding: 12px 20px;
+        display: block;
+
+        span {
+          font: normal normal 600 21px/28px Open Sans, sans-serif;
+        }
+      }
+    }
+  }
+
+  @media (min-width: 1024px) {
+    position: static;
+    width: 100%;
+    height: 92px;
+    overflow: hidden;
+    background-color: #fff;
+    color: #d6a430;
+    flex-direction: row-reverse;
+
+    ul {
+      display: flex;
+      align-items: center;
+
+      li {
+        font-size: 20px;
+        border-bottom: none;
+        text-transform: none;
+
+        a {
+          width: 100%;
+          padding: 12px 20px;
+          display: block;
+
+          span {
+            font: normal normal 600 20px/27px Open Sans, sans-serif;
+            display: inline-block;
+            text-align: justify;
+            text-align-last: justify;
+            letter-spacing: 0em;
+          }
+
+          span:first-child() {
+            width: 56px;
+          }
+        }
+      }
+      li:last-child {
+        a {
+          padding: 12px 0 12px 20px;
+        }
+      }
+    }
+  }
+`;
+
+export const ContentsMenu = styled.div`
+  text-align: center;
+
+  span.textMenu: first-child {
+    display: block;
+    font: normal normal bold 15px/20px Open Sans, sans-serif;
+  }
+
+  span.textMenu: nth-child(2) {
+    height: 34px;
+    width: 100%;
+    display: block;
+    font: normal normal bold 25px/34px Open Sans, sans-serif;
+  }
+
+  span.finalText {
+    height: 17px;
+    width: 100%;
+    display: block;
+    font: normal normal normal 12px/17px Open Sans, sans-serif;
+    text-transform: initial;
+  }
+
+  .buttonGroup {
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  @media (min-width: 1024px) {
+    display: none;
+  }
+`;
